b40445a33b4e51b09bac09d9dd2357f46b7f28df
Daniel Offei <archimedes301@gmail.com>
2025-10-19 13:09:27 +0000

    Modify script to add the description of the table

--- task_4.sql
+++ task_4.sql
@@ -1,8 +1,10 @@
 USE alx_book_store
 
-SELECT 
-    * 
+SELECT COLUMN_NAME, COLUMN_TYPE
+
 FROM INFORMATION_SCHEMA.COLUMNS
 
-WHERE TABLE_NAME = "books";
+WHERE TABLE_SCHEMA = 'alx_book_store'
+
+AND TABLE_NAME = "books";
 
